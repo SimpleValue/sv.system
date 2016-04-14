99d792af9da4693abdf758b7a7f43bed5a172659
Maximilian Weber <weber.maximilian@googlemail.com>
2016-04-14 17:24:37 +0200

    Moved transact-schema and transact-basic-data into one function.

--- src/sv/system/datomic/core.clj
+++ src/sv/system/datomic/core.clj
@@ -15,38 +15,38 @@
            (get-in config [:datomic :uri])]
    :stop d/release})
 
-(defn transact-schema [con schema]
-  @(d/transact
-    con
-    schema)
-  true)
+(defn prepare-tx-data [db-part tx-data]
+  (map
+   (fn [entry]
+     (if-not (:db/id entry)
+       (assoc entry :db/id (d/tempid db-part))
+       entry))
+   tx-data))
+
+(defn- transact [prepare-fn con tx]
+  (doseq [tx-data (if (list? tx)
+                    tx
+                    (list tx))]
+    @(d/transact con (prepare-fn tx-data))))
 
 (defn schema [config]
   (when (:dev config)
     {:binds [:datomic ::schema-transacted]
-     :start [transact-schema [:datomic :con] (-> config :datomic :schema)]}))
-
-(defn prepare-tx-data [tx-data]
-  (map
-   (fn [entry]
-     (if-not (:db/id entry)
-       (assoc entry :db/id (d/tempid :db.part/user))
-       entry))
-   tx-data))
-
-(defn transact-basic-data [con basic-data]
-  @(d/transact
-    con
-    (prepare-tx-data basic-data))
-  true)
+     :start [transact
+             (partial prepare-tx-data :db.part/db)
+             [:datomic :con]
+             (-> config :datomic :schema)]}))
 
 (defn basic-data [config]
   (when (:dev config)
     {:binds [:datomic ::basic-data]
-     :start [transact-basic-data [:datomic :con]
+     :start [transact
+             (partial prepare-tx-data :db.part/user)
+             [:datomic :con]
              (-> config :datomic :basic-data)]}))
 
 (defn queue [config]
   {:binds [:datomic :queue]
    :start [d/tx-report-queue [:datomic :con]]
    :stop [d/remove-tx-report-queue [:datomic :con]]})
+
